942c918e526f9895cd9bdabeb623b23956667455
yogaanand2805 <yogaanand2805@gmail.com>
2026-01-11 15:13:16 +0530

    Add files via upload

---
+++ SQL-Menu for resturant.sql
@@ -0,0 +1,30 @@
+
+
+
+create table Velu_kitchen(
+no int primary key auto_increment,
+item varchar(50),
+menu varchar(50),
+price double);
+
+insert into velu_kitchen (item,menu,price) values('tiffen','idly-2','50'),
+('tiffen','dosa','75'),
+('tiffen','pongal','60'),
+('tiffen','vada','20'),
+('tiffen','puri-3','60'),
+('lunch','veg meals','100'),
+('lunch','non-veg meals','150'),
+('lunch','biryani','150'),
+('lunch','fried rice','140'),
+('lunch','chilli chicken','180'),
+('lunch','chicken 65','180'),
+('dinner','dosa','75'),
+('dinner','parotta','75'),
+('dinner','fried rice','140'),
+('dinner','chilli chicken','180'),
+('dinner','chappathi','70'),
+('dinner','ommelte','35'),
+('sunday spl','mutton biryani','270'),
+('sunday spl','nattu kolli','280');
+
+
